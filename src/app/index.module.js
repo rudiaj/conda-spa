e0edc8e0babe63ecee155d204789b74d4efa0439
Dajana Obradovic <Dajana Obradovic>
2017-01-20 09:28:44 +0100

    ms

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('condaSpa', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'restangular', 'ngRoute', 'ui.bootstrap', 'toastr']);
+
+})();
---
+++ src/app/main/main.controller.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('condaSpa')
+    .controller('MainController', MainController);
+
+  /** @ngInject */
+  function MainController($timeout, webDevTec, toastr, $scope) {
+    var vm = this;
+    $scope.nesto ="kkeke";
+
+  
+  }
+})();
