f1d1c89ab4950959d79df423dda951a7c0671172
Dajana Obradovic <Dajana Obradovic>
2017-01-23 13:03:02 +0100

    fetched some data, todo:create filter to check html

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -3,27 +3,28 @@
 
 	angular
 	.module('condaSpa')
-	.controller('MainController', MainController);
+    .controller('MainController', MainController);
 
-	/** @ngInject */
-	function MainController($timeout, webDevTec, toastr, $scope, Restangular) {
-    // var vm = this;;
-    // vm.nesto ="fjkfjtg";
+    /** @ngInject */
+    function MainController($timeout, webDevTec, toastr, $scope, Restangular) {
 
-
-    // $scope.asset={};
+    $scope.asset=[];
 
     Restangular.setBaseUrl('https://dev-api.conda.online/assets/');
 
     Restangular.one('v1/de/','3790da38-6ff9-4749-b7a4-c42625bbcef3')
     .get()
     .then(function(result){
-        $scope.asset = result;
-        console.dir(result.asset_data);
+        $scope.asset = result.asset_data;
+        console.dir($scope.asset);
     });
 
 
-   
+
+    
+
+
+
     // tabs logic
     $scope.tab = 1;
 
